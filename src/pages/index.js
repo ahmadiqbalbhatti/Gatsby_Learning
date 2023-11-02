4c7d4be40d86842bbc39d6691825747ee04cd9a7
Ahmed Iqbal Bhatti <ahmad786687iqbal@gmail.com>
2023-11-03 00:09:43 +0500

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
       />
-      <StaticImage src={"../images/icon.png"} alt={"logo" }/>
+      {/*<StaticImage src={"../images/icon.png"} alt={"logo" }/>*/}
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
   );
